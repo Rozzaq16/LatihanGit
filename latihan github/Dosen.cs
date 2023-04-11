ea1f1e1f7aba28f0320c1fcb225418077461bb70
Rozzaq16 <murdhingabdurrozzaq@students.amikom.ac.id>
2023-04-11 22:05:21 +0700

    first commit

---
+++ latihan github/Dosen.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace latihan_github
+{
+    class Dosen
+    {
+        private int id;
+        private string nama;
+        private int Nik;
+        private string Gender;
+        private string Course;
+
+        public Dosen()
+        {
+            id = 1;
+            nama = "Dosen Pengampu";
+            Nik = 123456789;
+            Gender = "Male";
+            Course = "Pemrograman Berorientasi Objek";
+        }
+
+        public void attDosen(string name, int nik)
+        {
+            nama = name;
+            Nik = nik;
+        }
+
+        public void addCourse(string course)
+        {
+            Course = course;
+        }
+
+        public void display()
+        {
+            Console.WriteLine("ID\t" + id);
+            Console.WriteLine("Nama\t: " + nama);
+            Console.WriteLine("Nik\t " + Nik);
+            Console.WriteLine("Course\t " + Course);
+            Console.WriteLine();
+        }
+
+
+    }
+}
---
+++ latihan github/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace latihan_github
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dosen dosen = new Dosen();
+            Console.WriteLine("default Dosen pengampu!");
+            dosen.display();
+
+            Console.WriteLine("\nMasukan Nama dan NIK Dosen Baru:");
+            dosen.attDosen(Console.ReadLine(), Convert.ToInt32(Console.ReadLine()));
+            dosen.display();
+
+            Console.WriteLine("\nMasukan Mata Kuliah Baru:");
+            dosen.addCourse(Console.ReadLine());
+            dosen.display();
+
+            Console.ReadLine();
+
+        }
+    }
+}
